6717c8b965a6971b169171261c1bf922551102f0
zumrud7 <zumrudia@code.edu.az>
2021-06-11 20:29:04 +0400

    forms updated

--- src/newSubscription/index.tsx
+++ src/newSubscription/index.tsx
@@ -40,8 +40,11 @@ export const NewSubscription = () => {
     new Date("2014-08-18T21:11:54")
   );
 
+  const [formState, setFormState] = React.useState({});
+
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
+    console.log(selectedDate);
   };
 
   const [open, setOpen] = React.useState(false);
@@ -69,55 +72,64 @@ export const NewSubscription = () => {
         <DialogTitle style={{ color: "#194949" }} id="alert-dialog-title">
           Add New Subscription
         </DialogTitle>
-        <DialogContent>
-          <form style={{ display: "flex", flexDirection: "column" }}>
-            <FormControl className={classes.formWrap}>
-              <InputLabel className={classes.label} htmlFor="add_company-name">
-                Company Name
-              </InputLabel>
-              <Input className={classes.input} id="add_company-name" />
-            </FormControl>
-            <FormControl className={classes.formWrap}>
-              <InputLabel className={classes.label} htmlFor="add_email">
-                Email address
-              </InputLabel>
-              <Input id="add_email" />
-            </FormControl>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <KeyboardDatePicker
-                margin="normal"
-                id="sub_date"
-                label="Subscription date"
-                className={classes.label}
-                format="MM/dd/yyyy"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-              />
-            </MuiPickersUtilsProvider>
-            <FormControl className={classes.formWrap}>
-              <InputLabel className={classes.label} htmlFor="add_amount">
-                Amount
-              </InputLabel>
-              <Input id="add_amount" />
-            </FormControl>
-          </form>
-        </DialogContent>
-        <DialogActions>
-          <Button type="submit" variant="contained" color="primary">
-            Add subscription
-          </Button>
-          <Button
-            //   onClick={handleDialogClose}
-            variant="contained"
-            color="secondary"
-            autoFocus
-          >
-            Close
-          </Button>
-        </DialogActions>
+        <form
+          style={{ display: "flex", flexDirection: "column", padding: "0 15px" }}
+        >
+          <FormControl className={classes.formWrap}>
+            <InputLabel className={classes.label} htmlFor="add_company-name">
+              Company Name
+            </InputLabel>
+            <Input
+              className={classes.input}
+              id="add_company-name"
+            />
+          </FormControl>
+          <FormControl className={classes.formWrap}>
+            <InputLabel className={classes.label} htmlFor="add_email">
+              Email address
+            </InputLabel>
+            <Input 
+            id="add_email" 
+            className={classes.input}
+            />
+          </FormControl>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              margin="normal"
+              id="sub_date"
+              label="Subscription date"
+              className={classes.label}
+              format="MM/dd/yyyy"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </MuiPickersUtilsProvider>
+          <FormControl className={classes.formWrap}>
+            <InputLabel className={classes.label} htmlFor="add_amount">
+              Amount
+            </InputLabel>
+            <Input 
+            id="add_amount" 
+            className={classes.input}
+            />
+          </FormControl>
+          <DialogActions>
+            <Button type="submit" variant="contained" color="primary">
+              Add subscription
+            </Button>
+            <Button
+              onClick={handleClose}
+              variant="contained"
+              color="secondary"
+              autoFocus
+            >
+              Close
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </>
   );
