aee45c6818af0e206cd088f5335fb0f6d7dff60f
zumrud7 <zumrudia@code.edu.az>
2021-06-11 12:54:54 +0400

    auth folder updated

--- src/Auth/components/auth/index.tsx
+++ src/Auth/components/auth/index.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import "./index.scss";
-import { BrowserRouter, Link, Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { RegisterForm } from "./registerForm";
 import { LoginForm } from "./loginForm";
 
-
-// const useStyle = makeStyles({
-//   container: {
-//     width: "100%",
-//     maxWidth: "500px",
-//     background: "#fff",
-//   },
-//   input: {
-//     border: "1px solid black",
-//   },
-// });
 
 export const Auth: React.FC = ({}) => {
   return (
