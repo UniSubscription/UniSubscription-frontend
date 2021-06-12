504b56a842a922568918636c770940ebd0c6b99e
zumrud7 <zumrudia@code.edu.az>
2021-06-12 10:59:43 +0400

    not found page is created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Auth } from "./Auth/components/auth";
 import { Subscription } from "./Subscription/components";
+import { NotFound } from "./not-found";
 
 function App() {
   return (
@@ -10,9 +11,8 @@ function App() {
       <Switch>
         <Route path="/user" component={Auth} />
         <Route exact path="/subscription" component={Subscription} />
-        <Route path="*">
-          <Redirect to="/user" />
-        </Route>
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="not-found" />
       </Switch>
     </BrowserRouter>
   );
